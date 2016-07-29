2ce93085953435b15047dab9cef27e875e1fd9ed
rnikolaus <rnikolaus@users.noreply.github.com>
2016-07-29 17:16:52 +0200

    minor change

--- osm/src/main/java/rnikolaus/osm/format/OsmRelation.java
+++ osm/src/main/java/rnikolaus/osm/format/OsmRelation.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rnikolaus.osm.format;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import org.json.simple.JSONObject;
 import org.xml.sax.Attributes;
